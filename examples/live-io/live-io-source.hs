fab89c63f36a1b4916a1fd6933f51f02dab5df82
Alan Hawkins <hawk.alan@gmail.com>
2014-01-13 10:57:05 +1100

    reset example

--- examples/live-io/live-io-source.hs
+++ examples/live-io/live-io-source.hs
@@ -1,19 +1,4 @@
-import Control.Monad.Reader
+liveMain = do 
+       print 1
 
-
-{-# LANGUAGE NoMonomorphismRestriction #-}
-
-inc = (+1)
-dec = (subtract 1)
-
-add 0 b = b
-add a b = add (dec a) (inc b)
-
-multiply a 0 = 0
-multiply a b = add a (multiply a (dec b))
-
-power a 0 = 1
-power a b = multiply a (power a (dec b))
-
-
-liveMain =  print $ power 5 5
+        
